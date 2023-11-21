13f836105fe8161ca7cd40e09a487e9b4637b2bb
toiQS <88361704+toiQS@users.noreply.github.com>
2023-11-21 17:00:35 +0700

    Update demoHash.js
    demo sơ qua cấu trúc, tui cảm thấy nó đơn giản với hiệu quả cũng tương tự với bảng băm

--- demoHash.js
+++ demoHash.js
@@ -1,29 +1,18 @@
 class demoHash{
-    constructor(text){
-        this.text = text;
-        this.array = new Array();
-    }
-    //chia dữ liệu đầu vào thành cách câu
-    splitDataInToSentencer(){
-        this.array = this.text.split(".");
-        return this.array;
-    }
-    //băm dữ liệu sau khi chia động với cách câu
-    splitData(){
-        
-        this.array.forEach(element => {
-            var arr2 = new Map();
-            arr2 = element.split(" ");
-            this.array.push(arr2);
-
-        });
-        return this.array;
-    }
+  constructor(text){
+    this.text = text;
+    this.array = new Array();
+  }
+  addSentencer(){
+    this.array = this.text.split(" ");
+    return this.array;
+  }
+  CheckSentencer(){
+    this.array.forEach(element => {
+      if(element.includes("#")){
+        return true;
+      }
+    });
+    return false;
+  }
 }
-var result = new demoHash("Đây không chỉ là một . khoảnh khắc đẹp của thiên nhiên, mà còn là một trạng thái tâm hồn, nơi mà con người và tự nhiên hòa quyện vào một đồng nhất hòa bình. Chắc chắn, những khoảnh khắc như thế. này là những khoảnh khắc đáng trân trọng, những kh.oảnh khắc mà tâm hồn ta thường mong đợi để tìm lại chính mình giữa cuộc sống hối hả.");
-var demo = result.splitDataInToSentencer();
-demo.forEach(element => {
-    console.log(element);
-})
-console.log("result 2: ");
-console.log(result.splitData());
